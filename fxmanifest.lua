9740b96c4d808c71f19aa4b904bae2f697d2a550
Re1ease <86981407+Re1ease@users.noreply.github.com>
2021-11-11 21:30:12 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+author "Re1ease"
+version '1.0.0'
+
+server_script {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended',
+}
