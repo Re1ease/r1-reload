24a3b6beb37f0b9ba509ddd61c3b2c8a7f4eaf06
Re1ease <86981407+Re1ease@users.noreply.github.com>
2021-11-11 21:37:25 +0100

    Update config.lua

--- r1_reload/config.lua
+++ r1_reload/config.lua
@@ -5,7 +5,7 @@ Config.Framework = "ESX"                --"ESX" or "STANDALONE"
 Config.Debug = false                    --Displays information about the current weapon (prints to console).
 Config.DebugRefresh = 5000              --How often should it refresh.
 
-Config.Item = "pistol_clip"             --Database name of YOUR item that should be used as a weapon clip.
+Config.Item = "CHANGE_ME"             --Database name of YOUR item that should be used as a weapon clip.
 
 Config.SpamDelay = 1500                 --If trigger and error; How long delay until next trigger.
 Config.CheckAndApplyAmmo = true         --Weapons that have ammo beyond the clip gets its ammo automatically removed.
